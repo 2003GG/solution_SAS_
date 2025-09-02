a57d7fa645f3fddcd7d9496ab2e59de39ecd516f
2003GG <mohamadzat6@gmail.com>
2025-09-02 15:30:29 +0200

    the second commit

--- chapitre4_7.c
+++ chapitre4_7.c
@@ -7,18 +7,17 @@ printf("le montant en dollars : ");
 scanf("%d",&d);
 
  k = d/20;
- X=k/10;
- z=X/5;
- y=z/1;
+ X=(d%20)/10;
+ y=(d%10)/5;
+ z=(d%5)/1;
+ 
+
 
  printf("Entrez un montant en dollars : %d\n",d);
  printf("Billets de 20$ : %d\n",k);
  printf("Billets de 10$ : %d\n",X);
- printf("Billets de 5$ : %d\n",z);
- printf(" Billets de 1$ : %d\n",y);
+ printf("Billets de 5$ : %d\n",y);
+ printf(" Billets de 1$ : %d\n",z);
+
     return 0;
 }
-// Billets de 20$ : 4
-// Billets de 10$ : 1
-// Billets de 5$ : 0
-// Billets de 1$ : 3
